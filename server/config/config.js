21a4c0c070d4466db97ed4610f63b52b2a178eba
Cecilia Perez Garcia <cecilia.perez@TIERRAYARMONIA.local>
2019-10-03 12:50:30 -0500

    Variable de entonrno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cecilia:FyNyCW1fWvVHhrBs@cluster0-7orgw.mongodb.net/cafe';
+    urlDB = process.env.NODE_ENV.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
