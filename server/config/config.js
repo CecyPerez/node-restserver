3ea9f3a61ecd4eb6870594c7341586099dd25b42
Cecilia Perez Garcia <cecilia.perez@TIERRAYARMONIA.local>
2019-10-03 12:14:21 -0500

    RestSever - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//BD
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cecilia:FyNyCW1fWvVHhrBs@cluster0-7orgw.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
